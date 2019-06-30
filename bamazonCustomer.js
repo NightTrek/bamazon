e6fc8c60ebc6972d940281b3a85db122d13861e3
Daniels@dual4t.com <daniels@dual4t.com>
2019-06-30 16:25:35 -0700

    added promise based structure for commands

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,7 +8,9 @@
 const E = require('enquirer');
 const mysql = require("mysql2/promise");
 
-const getConnection = async function () {
+
+const GetConnection = async function(){
+try{
     return mysql.createConnection({
         host: "localhost",
 
@@ -23,63 +25,71 @@ const getConnection = async function () {
         database: "bamazon_db"
     });
 }
-
-const ItemCon = function(id, name ){
-    
+catch(err){
+ throw err;
 }
 
-const displayItemsForSale = async function (con) {
-    try {
+}
+
+const DisplayItemsForSale = async function(con){
+    try{
         const response = await con.query(`SELECT * FROM products`);
-        let ItemArray = [];
-        // console.log('----------------------------------------------')
-         for (key in response[0]) {
-            // console.log(`id: ${response[0][key].item_id} || Product Name: ${response[0][key].product_name} || Department: ${response[0][key].department} || Price: ${response[0][key].price} ||`) 
-            // console.log('------------------------------------------------------------------------------------')
-            ItemArray.push(`id: ${response[0][key].item_id} || Product Name: ${response[0][key].product_name} || Department: ${response[0][key].department} || Price: ${response[0][key].price} ||`);
-        }
 
-        try{
-            console.log(ItemArray)
-            let response = await E
-        .prompt([
-            // Here we create a basic text prompt.
-            {
-                type: "list",
-                message: "Which product do you want to buy",
-                choices: ItemArray,
-                name: "product" 
-            },
-            // Here we create a basic password-protected text prompt.
-            {
-                type: "input",
-                message: "Command Options or search term?",
-                name: "options"
+        return new Promise((resolve, reject) => {
+            if(response) {
+                let ItemArray = [];
+                for (key in response[0]) {
+                    console.log(`id: ${response[0][key].item_id} || Product Name: ${response[0][key].product_name} || Department: ${response[0][key].department} || Price: ${response[0][key].price} ||`) 
+                    console.log('------------------------------------------------------------------------------------')
+                    ItemArray.push(`id: ${response[0][key].item_id} || Product Name: ${response[0][key].product_name} || Department: ${response[0][key].department} || Price: ${response[0][key].price} ||`);
+                }
+               resolve(ItemArray); 
+            } else {
+               const errorObject = {
+                  msg: 'An error occured displaying items',
+                  error, //...some error we got back
+               }
+               reject(errorObject);
             }
-        ])// end of await
-
-
-        console.log(response)
-        }
-        catch(err){
-            console.log(err)
-        }
+         });
     }
     catch(err){
         throw err;
     }
-}//i wonder
+}
+
+const getCustomerOrder = async function(){
+    try{
+    let response = await E
+    .prompt([
+        // Here we create a basic text prompt.
+        {
+            type: "input",
+            message: "List the Item ID's you wish you order seperated by comas",
+            choices: ItemArray,
+            name: "product" 
+        },
+        // Here we create a basic password-protected text prompt.
+        {
+            type: "input",
+            message: "List the ammounts seperated by comas?",
+            name: "Amounts"
+        }
+])// end of await
+    }
+    catch(err){
+        throw err;
+    }
+}
 
 const interfaceStart = async function () {
-    const Connect = await getConnection();
-    displayItemsForSale(Connect)
-
-
-
-
+    const con = await GetConnection()
+    const ItemArray = await DisplayItemsForSale(con)
     
+   
     
-
+  
 }
 
 interfaceStart();
+
